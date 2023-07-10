74602fa368fca434167832d7a8088e95a5949630
deepika2497 <135425717+deepika2497@users.noreply.github.com>
2023-07-10 15:56:46 +0530

    Update main.tf

--- terraform-scripts/main.tf
+++ terraform-scripts/main.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
+      version = "5.7.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = var.region
 }
