590f62b90b1da084e0a906049f1ed4d8bb171372
deepika2497 <135425717+deepika2497@users.noreply.github.com>
2023-07-10 16:00:06 +0530

    Create securitygroup.tf

---
+++ terraform-scripts/securitygroup.tf
@@ -0,0 +1,58 @@
+resource "aws_security_group" "public_sg" {
+  name        = "PublicSecurityGroup"
+  description = "Security group for public subnet"
+  vpc_id      = aws_vpc.vprofileapp_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.public_sg_tags
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "PrivateSecurityGroup"
+  description = "Security group for private subnet"
+  vpc_id      = aws_vpc.vprofileapp_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.private_sg_tags
+}
